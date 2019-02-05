fdedcc8131e2c54ae54e8788a323088e74930cb0
Major Major <mtpogue@gmail.com>
2019-02-04 22:31:48 -0800

    MVVM And can track peices

---
+++ BoomTracker/TetrisWindow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoomTracker
+{
+	public static class TetrisWindow
+	{
+
+
+		public static class PlayingField
+		{
+			public static int TopPixel => 421;
+			public static int BottomPixel => 263;
+			public static int LeftPixel => 260;
+			public static int RightPixel => 481;
+
+		
+		}
+	}
+}
